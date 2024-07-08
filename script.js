049ce536c7d9e4ce1948e4381c202d7bccc910db
Simran-parashar <162283331+Simran-parashar@users.noreply.github.com>
2024-07-08 15:40:49 +0530

    Add files via upload
    Personal Portfolio Website
    
    This repository contains the code for my personal portfolio website, crafted with HTML, CSS, and JavaScript. It's designed to highlight my skills, projects, and background in a visually appealing and user-friendly manner. The website features a modern, responsive layout using CSS for seamless navigation across devices. JavaScript is utilized for interactive elements like animated transitions, and form validation for the contact section. 
    
    Key Features:
    
    Responsive design optimized for various screen sizes
    Smooth animations and transitions using vanilla JavaScript
    Contact form with client-side validation for improved user experience

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    // Show/hide scroll up button
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // Slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+    });
+
+    // Toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // Typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Frontend Developer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // Owl Carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
